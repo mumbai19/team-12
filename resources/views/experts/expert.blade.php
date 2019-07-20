bdc0976ee533f03a1058fc3626b076bcca1dab80
guptapradyumn <guptapradyumn284@gmail.com>
2019-07-20 19:16:55 +0000

    migration

--- resources/views/experts/expert.blade.php
+++ resources/views/experts/expert.blade.php
@@ -51,19 +51,50 @@
                 <div class="modal-dialog">
               
                   <!-- Modal content-->
+                  <form>
                   <div class="modal-content">
                     <div class="modal-header">
+                        <h4 class="modal-title">Add Video</h4>
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      <h4 class="modal-title">Add Video</h4>
                     </div>
                     <div class="modal-body">
-                      <p>Some text in the modal.</p>
+                        <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Link to YouTube Video" name="link">
+                        </div>
+                        <div class="form-group">
+                                <input type="text" class="form-control" placeholder="add types" name="type">
+                        </div>
                     </div>
                     <div class="modal-footer">
+                        <button type="button" class="btn btn-default" >Submit</button>
                       <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     </div>
                   </div>
-              
+                </form>
                 </div>
-              </div>
+ </div>
+
+ <div id="advice" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+              
+                  <!-- Modal content-->
+                  <form>
+                  <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Give Advice</h4>
+                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Advice...." name="advice">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" >Submit</button>
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </form>
+                </div>
+ </div>
 @endsection
