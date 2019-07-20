7d6440ef03baf957b8e8f8ed54328ee8599cf982
Raj Chandvaniya <rajchandvaniya@gmail.com>
2019-07-20 18:56:19 +0530

    Farmer Sale Post Added

---
+++ resources/views/farmer/pages/sale.blade.php
@@ -0,0 +1,19 @@
+@extends('farmer.dashboard.Nav1')
+@section('content')
+<section class="content">
+<form action="/action_page.php">
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</section>
+@stop
